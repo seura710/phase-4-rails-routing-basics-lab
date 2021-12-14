4b939757bc3194e910f241d38ef01701d1574ca4
Harris Kim <seura710@gmail.com>
2021-12-14 17:20:15 -0500

    Completed Test

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,8 +5,13 @@ class StudentsController < ApplicationController
     end
 
     def grades
-        grades=Students.all
-
+        grades=Student.order(grade: :desc)
         render json: grades
     end
+    # Students GET /students/grades returns an array of all students ordered by grade
+
+    def highest_grades
+        grades=Student.order(grade: :desc).first
+        render json: grades
+    end 
 end
